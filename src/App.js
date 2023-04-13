3bfc340a88a2c000373c9e9c02bf4aab8ce98fae
jambu moses <jambcareer@gmail.com>
2023-04-13 20:15:49 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+/* imports */
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+/* components */
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+
+/* pages */
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+
+/* styles */
+import "./Styles/main.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/home" component={Home} />
+          <Route path="/index" component={Home} />
+
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+
+          {/* 404 */}
+          <Route path="*" component={Home} />
+        </Switch>
+
+        <Footer />
+      </Router>
+    </>
   );
 }
 
