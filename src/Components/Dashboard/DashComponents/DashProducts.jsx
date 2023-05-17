40b0fc3f81fdb421343987d43078fc608517e23c
jambu moses <jambcareer@gmail.com>
2023-05-17 19:15:26 +0300

    dashboard adding product list

--- src/Components/Dashboard/DashComponents/DashProducts.jsx
+++ src/Components/Dashboard/DashComponents/DashProducts.jsx
@@ -5,29 +5,306 @@ export default function DashProducts() {
     <>
       <div className="DashHeader">
         <h2>products</h2>
+        <div>
+          <input
+            style={{
+              padding: "1px 10px",
+              borderRadius: "5px",
+              outline: "none",
+              border: "2px outset black",
+              textTransform: "capitalize",
+              fontSize: "13px",
+            }}
+            type="button"
+            value="add item"
+          />
+        </div>
       </div>
       <section className="DashContentContainer">
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
+        <div
+          style={{ backgroundColor: "grey" }}
+          className="dashContentItem-list dashContentProduct-list"
+        >
+          <span className="dashContentProduct-list-thumbnail"></span>
+          <span className="dashContentProduct-list-title">Product</span>
+          <span className="dashContentProduct-list-category">Category</span>
+          <span className="dashContentProduct-list-description">
+            Description
+          </span>
+          <span className="dashContentProduct-list-rating">Rating</span>
+          <span className="dashContentProduct-list-price">Price</span>
+          <span className="dashContentProduct-list-buttons">Action</span>
+        </div>
+
+        <div className="dashContentItem-list dashContentProduct-list">
+          <span className="dashContentProduct-list-thumbnail"></span>
+          <span className="dashContentProduct-list-title">Acer Aspire R3</span>
+          <span className="dashContentProduct-list-category">
+            Game Consoles
+          </span>
+          <span className="dashContentProduct-list-description">
+            Premuim Quad Core 4th Gen 14 GB/500 GB HDD/Win00 A
+          </span>
+          <span className="dashContentProduct-list-rating">
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+          </span>
+          <span className="dashContentProduct-list-price">$ 2050</span>
+          <span className="dashContentProduct-list-buttons">
+            <button>
+              <i className="fa fa-save"></i>
+            </button>
+            <button>
+              <i className="fa fa-save"></i>
+            </button>
+          </span>
+        </div>
+
+        <div className="dashContentItem-list dashContentProduct-list">
+          <span className="dashContentProduct-list-thumbnail"></span>
+          <span className="dashContentProduct-list-title">Acer Aspire R3</span>
+          <span className="dashContentProduct-list-category">
+            Game Consoles
+          </span>
+          <span className="dashContentProduct-list-description">
+            Premuim Quad Core 4th Gen 14 GB/500 GB HDD/Win00 A
+          </span>
+          <span className="dashContentProduct-list-rating">
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+          </span>
+          <span className="dashContentProduct-list-price">$ 2050</span>
+          <span className="dashContentProduct-list-buttons">
+            <button>
+              <i className="fa fa-save"></i>
+            </button>
+            <button>
+              <i className="fa fa-save"></i>
+            </button>
+          </span>
+        </div>
+
+        <div className="dashContentItem-list dashContentProduct-list">
+          <span className="dashContentProduct-list-thumbnail"></span>
+          <span className="dashContentProduct-list-title">Acer Aspire R3</span>
+          <span className="dashContentProduct-list-category">
+            Game Consoles
+          </span>
+          <span className="dashContentProduct-list-description">
+            Premuim Quad Core 4th Gen 14 GB/500 GB HDD/Win00 A
+          </span>
+          <span className="dashContentProduct-list-rating">
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+          </span>
+          <span className="dashContentProduct-list-price">$ 2050</span>
+          <span className="dashContentProduct-list-buttons">
+            <button>
+              <i className="fa fa-save"></i>
+            </button>
+            <button>
+              <i className="fa fa-save"></i>
+            </button>
+          </span>
+        </div>
+
+        <div className="dashContentItem-list dashContentProduct-list">
+          <span className="dashContentProduct-list-thumbnail"></span>
+          <span className="dashContentProduct-list-title">Acer Aspire R3</span>
+          <span className="dashContentProduct-list-category">
+            Game Consoles
+          </span>
+          <span className="dashContentProduct-list-description">
+            Premuim Quad Core 4th Gen 14 GB/500 GB HDD/Win00 A
+          </span>
+          <span className="dashContentProduct-list-rating">
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+          </span>
+          <span className="dashContentProduct-list-price">$ 2050</span>
+          <span className="dashContentProduct-list-buttons">
+            <button>
+              <i className="fa fa-save"></i>
+            </button>
+            <button>
+              <i className="fa fa-save"></i>
+            </button>
+          </span>
+        </div>
+
+        <div className="dashContentItem-list dashContentProduct-list">
+          <span className="dashContentProduct-list-thumbnail"></span>
+          <span className="dashContentProduct-list-title">Acer Aspire R3</span>
+          <span className="dashContentProduct-list-category">
+            Game Consoles
+          </span>
+          <span className="dashContentProduct-list-description">
+            Premuim Quad Core 4th Gen 14 GB/500 GB HDD/Win00 A
+          </span>
+          <span className="dashContentProduct-list-rating">
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+          </span>
+          <span className="dashContentProduct-list-price">$ 2050</span>
+          <span className="dashContentProduct-list-buttons">
+            <button>
+              <i className="fa fa-save"></i>
+            </button>
+            <button>
+              <i className="fa fa-save"></i>
+            </button>
+          </span>
+        </div>
+
+        <div className="dashContentItem-list dashContentProduct-list">
+          <span className="dashContentProduct-list-thumbnail"></span>
+          <span className="dashContentProduct-list-title">Acer Aspire R3</span>
+          <span className="dashContentProduct-list-category">
+            Game Consoles
+          </span>
+          <span className="dashContentProduct-list-description">
+            Premuim Quad Core 4th Gen 14 GB/500 GB HDD/Win00 A
+          </span>
+          <span className="dashContentProduct-list-rating">
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+          </span>
+          <span className="dashContentProduct-list-price">$ 2050</span>
+          <span className="dashContentProduct-list-buttons">
+            <button>
+              <i className="fa fa-save"></i>
+            </button>
+            <button>
+              <i className="fa fa-save"></i>
+            </button>
+          </span>
+        </div>
+
+        <div className="dashContentItem-list dashContentProduct-list">
+          <span className="dashContentProduct-list-thumbnail"></span>
+          <span className="dashContentProduct-list-title">Acer Aspire R3</span>
+          <span className="dashContentProduct-list-category">
+            Game Consoles
+          </span>
+          <span className="dashContentProduct-list-description">
+            Premuim Quad Core 4th Gen 14 GB/500 GB HDD/Win00 A
+          </span>
+          <span className="dashContentProduct-list-rating">
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+          </span>
+          <span className="dashContentProduct-list-price">$ 2050</span>
+          <span className="dashContentProduct-list-buttons">
+            <button>
+              <i className="fa fa-save"></i>
+            </button>
+            <button>
+              <i className="fa fa-save"></i>
+            </button>
+          </span>
+        </div>
+
+        <div className="dashContentItem-list dashContentProduct-list">
+          <span className="dashContentProduct-list-thumbnail"></span>
+          <span className="dashContentProduct-list-title">Acer Aspire R3</span>
+          <span className="dashContentProduct-list-category">
+            Game Consoles
+          </span>
+          <span className="dashContentProduct-list-description">
+            Premuim Quad Core 4th Gen 14 GB/500 GB HDD/Win00 A
+          </span>
+          <span className="dashContentProduct-list-rating">
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+          </span>
+          <span className="dashContentProduct-list-price">$ 2050</span>
+          <span className="dashContentProduct-list-buttons">
+            <button>
+              <i className="fa fa-save"></i>
+            </button>
+            <button>
+              <i className="fa fa-save"></i>
+            </button>
+          </span>
+        </div>
+
+        <div className="dashContentItem-list dashContentProduct-list">
+          <span className="dashContentProduct-list-thumbnail"></span>
+          <span className="dashContentProduct-list-title">Acer Aspire R3</span>
+          <span className="dashContentProduct-list-category">
+            Game Consoles
+          </span>
+          <span className="dashContentProduct-list-description">
+            Premuim Quad Core 4th Gen 14 GB/500 GB HDD/Win00 A
+          </span>
+          <span className="dashContentProduct-list-rating">
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+          </span>
+          <span className="dashContentProduct-list-price">$ 2050</span>
+          <span className="dashContentProduct-list-buttons">
+            <button>
+              <i className="fa fa-save"></i>
+            </button>
+            <button>
+              <i className="fa fa-save"></i>
+            </button>
+          </span>
+        </div>
+
+        <div className="dashContentItem-list dashContentProduct-list">
+          <span className="dashContentProduct-list-thumbnail"></span>
+          <span className="dashContentProduct-list-title">Acer Aspire R3</span>
+          <span className="dashContentProduct-list-category">
+            Game Consoles
+          </span>
+          <span className="dashContentProduct-list-description">
+            Premuim Quad Core 4th Gen 14 GB/500 GB HDD/Win00 A
+          </span>
+          <span className="dashContentProduct-list-rating">
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+            <i className="fa fa-star"></i>
+          </span>
+          <span className="dashContentProduct-list-price">$ 2050</span>
+          <span className="dashContentProduct-list-buttons">
+            <button>
+              <i className="fa fa-save"></i>
+            </button>
+            <button>
+              <i className="fa fa-save"></i>
+            </button>
+          </span>
+        </div>
       </section>
     </>
   );
