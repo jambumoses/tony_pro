7bd90938fb2c417319cad850a5c15191cfc120d3
jambu moses <jambcareer@gmail.com>
2023-05-18 23:59:20 +0300

    adding dashboard ads page

--- src/Components/Dashboard/DashComponents/DashAdverts.jsx
+++ src/Components/Dashboard/DashComponents/DashAdverts.jsx
@@ -4,30 +4,95 @@ export default function DashAdverts() {
   return (
     <>
       <div className="DashHeader">
-        <h2>advertisements (ads)</h2>
+        <h2>advertisements (<i className="fa fa-rectangle-ad"></i>)</h2>
+        <div style={{ display: "flex" }}>
+          <input
+            style={{
+              padding: "1px 10px",
+              borderRadius: "5px",
+              outline: "none",
+              border: "2px outset black",
+              textTransform: "capitalize",
+              fontSize: "13px",
+            }}
+            type="button"
+            value="create ads"
+          />
+        </div>
       </div>
       <section className="DashContentContainer">
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
+        <div className="ads-container">
+          <div className="ads-item">
+            
+            <span>
+              <img
+                src={require("../../../Assets/ads/UG_W4_JUMIA-LOVES-YOU_STICKYBANNER_1168x56.gif")}
+                alt=""
+              />
+            </span>
+            <span>
+              <img
+                src={require("../../../Assets/ads/UG_W4_JUMIA-LOVES-YOU_xoxoxoxooxox.gif")}
+                alt=""
+              />
+            </span>
+
+            <span>
+              <img
+                src={require("../../../Assets/ads/UG_W4_JUMIA-LOVES-YOU_xoxoxoxooxox.gif")}
+                alt=""
+              />
+            </span>
+
+            <span>
+              <img
+                src={require("../../../Assets/ads/UG_W4_JUMIA-LOVES-YOU_xoxoxoxooxox.gif")}
+                alt=""
+              />
+            </span>
+            <span>
+              <img
+                src={require("../../../Assets/ads/UG_W4_JUMIA-LOVES-YOU_STICKYBANNER_1168x56.gif")}
+                alt=""
+              />
+            </span>
+
+            <span>
+              <img
+                src={require("../../../Assets/ads/UG_W4_JUMIA-LOVES-YOU_STICKYBANNER_1168x56.gif")}
+                alt=""
+              />
+            </span>
+            <span>
+              <img
+                src={require("../../../Assets/ads/UG_W4_JUMIA-LOVES-YOU_xoxoxoxooxox.gif")}
+                alt=""
+              />
+            </span>
+
+            <span>
+              <img
+                src={require("../../../Assets/ads/UG_W4_JUMIA-LOVES-YOU_xoxoxoxooxox.gif")}
+                alt=""
+              />
+            </span>
+
+            <span>
+              <img
+                src={require("../../../Assets/ads/UG_W4_JUMIA-LOVES-YOU_xoxoxoxooxox.gif")}
+                alt=""
+              />
+            </span>
+            <span>
+              <img
+                src={require("../../../Assets/ads/UG_W4_JUMIA-LOVES-YOU_STICKYBANNER_1168x56.gif")}
+                alt=""
+              />
+            </span>
+
+
+          </div>
+        </div>
       </section>
     </>
   );
--- src/Components/Dashboard/DashComponents/DashSettings.jsx
+++ src/Components/Dashboard/DashComponents/DashSettings.jsx
@@ -7,27 +7,24 @@ export default function DashSettings() {
         <h2>settings</h2>
       </div>
       <section className="DashContentContainer">
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
-        <div>content</div>
+        <div>payment methods</div>
+        <div>{/* color scheme */}</div>
+        <div>*merchant logo</div>
+        <div>*merchant name</div>
+        <div>*merchant username</div>
+        <div>*merchant password</div>
+        <div>merchant services</div>
+        <div>merchant about</div>
+        <div>merchant socials</div>
+        <div>*merchant phone</div>
+        <div>*merchant email</div>
+        <div>*merchant date of birth</div>
+        <div>merchant policies</div>
+        <div>accounts/users/clients</div>
+        <div>merchant terms and condition</div>
+        <div>delivery/orders history</div>
+        <div>*feedbacks</div>
+        <div>*frequent searches</div>
       </section>
     </>
   );
